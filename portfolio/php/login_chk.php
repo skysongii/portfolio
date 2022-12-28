f53a5b6ffb75534eefa8af344b2800e3c3eb10a4
skysongii <tjsgh9414@naver.com>
2022-12-28 11:12:50 +0900

    short

--- portfolio/php/login_chk.php
+++ portfolio/php/login_chk.php
@@ -1,4 +1,4 @@
-<?php
+<?
 // error_reporting(E_ALL);
 // ini_set("display_errors", 1);
 // $db_chk = 0, 1       // conn.php 내 db 체크 값 (0: 실패, 1: 성공)
@@ -12,13 +12,13 @@ $ROOT_PATH = "/dashboard/src/portfolio";
 include_once $_SERVER["DOCUMENT_ROOT"] .$ROOT_PATH. "/php/conn.php";
 
 if($db_chk == 1) {
-
+    
 } else { ?>
     <script>
         alert("서버와의 통신 중 오류가 발생하였습니다.");
         history.back();
     </script>
- <?php }
+ <? }
 
 
 $select_query = "select id, password from sguser where id='$user_id' and password='$user_pwd'";
