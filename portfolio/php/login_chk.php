e8de90094a863237cdc6c7a47897a13940b2a88d
skysongii <tjsgh9414@naver.com>
2023-01-05 08:51:56 +0900

    리다이렉트

--- portfolio/php/login_chk.php
+++ portfolio/php/login_chk.php
@@ -34,8 +34,7 @@ if($row_num == 0) {
     echo "<script>alert('아이디 혹은 비밀번호가 일치하지 않습니다.')</script>";
     echo "<script>history.back()</script>";
 } else {
-    echo "<h1>$user_id</>";
-    echo "<h1>$user_pwd</>";
+    echo 
 }
 // echo $row_num;
 echo "<br>";
